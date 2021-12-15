630f938ac271e640c078c956f203aa2f73475895
igal1992 <80173737+igal1992@users.noreply.github.com>
2021-12-15 13:04:10 +0200

    heyaaaaa

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ app.use(cors({
 app.use(cookieParser())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:false}))
+app.set('trust proxy', 1)
 app.use(session({
     key:"user",
     secret:process.env.SECRET_SESSION_KEY,
