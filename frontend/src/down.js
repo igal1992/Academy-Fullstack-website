cb89bdfae89ef082b46b668ae8c0986e954042e5
igal1992 <80173737+igal1992@users.noreply.github.com>
2022-02-09 11:58:21 +0200

    Delete frontend directory

--- frontend/src/down.js
+++
@@ -1,9 +0,0 @@
-import { withRouter } from 'react-router'
-
-function down(){
-return(
-    <section id ={"servers_down"}>
-        <i class="fa fa-frown-o" aria-hidden="true"/><h1 id = {"servers_down_Text"}>Oops! Servers are Down!</h1>
-    </section>
-)}
-export default withRouter(down);
